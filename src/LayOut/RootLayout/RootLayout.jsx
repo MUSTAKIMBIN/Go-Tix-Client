484262c303752c79f4f10770bf56861b951a1528
MUSTAKIMBIN <mustakimbin24@gmail.com>
2025-12-12 15:07:16 +0600

    added footer component

--- src/LayOut/RootLayout/RootLayout.jsx
+++ src/LayOut/RootLayout/RootLayout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navber from "../../Components/Navber/Navber";
 import { Outlet } from "react-router";
+import Footer from "../../Components/Footer/Footer";
 
 const RootLayout = () => {
   return (
@@ -9,6 +10,7 @@ const RootLayout = () => {
       {/* added basics file setpus and router sertpus */}
       <Navber></Navber>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
